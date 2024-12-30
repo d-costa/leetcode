40dd0d4fc3acdc6a7cf8061850c8860c6efb3819
David Costa <davidamorimc@gmail.com>
2024-12-30 20:19:49 +0000

    0121-BEST-TIME-TO-BUY-AND-SELL-STOCK Stats: Time: 121 ms (32.37%), Space: 27.1 MB (8.32%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -5,18 +5,20 @@ class Solution:
         
         best_profit = 0
         last_price = prices[0]
-        curr_min = prices[0]
-        curr_max = prices[0]
+        local_min = prices[0]
+        local_max = prices[0]
     
         for i in range(1, len(prices)):
             curr_price = prices[i]
-            
-            if curr_price > last_price:
-                curr_max = curr_price
-            elif curr_price < last_price:
-                curr_max = curr_price
-                curr_min = min(curr_min, curr_price)
 
-            best_profit = max(best_profit, curr_max - curr_min)
+            local_max = curr_price # We cannot sell in the past
+            
+            if curr_price < last_price:
+                # But we can always now at a lower price!
+                local_min = min(local_min, curr_price)
+
+            # Save best profit
+            best_profit = max(best_profit, local_max - local_min)
+
             last_price = curr_price
         return best_profit
