38e4e5fafb9e0003bb63b4e2ab2276c85043607f
David Costa <davidamorimc@gmail.com>
2024-12-30 19:41:10 +0000

    0119-PASCALS-TRIANGLE-II Stats: Time: 0 ms (100%), Space: 17.7 MB (11.19%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,7 +1,4 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        result = []
-        for i in range(0, rowIndex + 1):
-            result.append(math.comb(rowIndex, i))
-        return result
+        return [math.comb(rowIndex, i) for i in range(0, rowIndex + 1)]
         
