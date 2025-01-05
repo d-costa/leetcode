381755f11bf6693e00557e3e240970476ad49f8c
David Costa <davidamorimc@gmail.com>
2025-01-05 11:28:09 +0000

    2465-SHIFTING-LETTERS-II Stats: Time: 61 ms (59.88%), Space: 41.3 MB (30.09%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.py
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.py
@@ -4,7 +4,6 @@ class Solution:
         diff_arr = [0] * (len(s) + 1)
 
         for shift in shifts:
-            print(shift)
             #approach 1
             diff_arr[shift[0]] = diff_arr[shift[0]] + (1 if shift[2] == 1 else -1)
             diff_arr[shift[1] + 1] = diff_arr[shift[1] + 1] + (- 1 if shift[2] == 1 else 1)
@@ -17,6 +16,5 @@ class Solution:
             # A = 97
             # Z = 122
             result = result + chr((ord(c) - 97 + prefix_sum) % (122 - 97 + 1) + 97)
-            print(f"{c} - {prefix_sum} = {result[len(result)-1]}")
 
         return result
