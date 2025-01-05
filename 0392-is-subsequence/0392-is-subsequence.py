2c66e3d63266e4f3a43849fc4e5ccd6961725490
David Costa <davidamorimc@gmail.com>
2025-01-05 12:14:12 +0000

    0392-IS-SUBSEQUENCE Stats: Time: 42 ms (3.78%), Space: 19.3 MB (7.98%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,14 +1,15 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        return aux(s, t)
-
-def aux(s, t):
-    if len(t) == 0:
-      return len(s) == 0
-    if len(s) == 0:
-        return True
+        def aux(s_i, t_i):
+            if t_i == len(t):
+                return s_i == len(s)
+            if s_i == len(s):
+                return True
+            
+            if s[s_i] == t[t_i]:
+                return aux(s_i + 1, t_i + 1)
+            else:
+                return aux(s_i, t_i + 1)
     
-    if s[0] == t[0]:
-        return aux(s[1:], t[1:])
-    else:
-        return aux(s, t[1:])
+        return aux(0, 0)
+
