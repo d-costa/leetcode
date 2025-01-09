48ea54f1c62525458eb315e176b0e62470b79316
David Costa <davidamorimc@gmail.com>
2025-01-09 00:56:04 +0000

    1524-STRING-MATCHING-IN-AN-ARRAY Stats: Time: 79 ms (5.02%), Space: 17.8 MB (34.66%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -1,8 +1,55 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        result = []
+        found = []
         for word in words:
-            if any((word != w and word in w) for w in words):
-                result.append(word)
-        return result
+            pattern = substring_search(word)
+            for w in words:
+                if word != w and len(word) < len(w) and (not word in found) and compare_match(word, w, pattern):
+                    found.append(word)
+        return found
+
+
+# true if word is substring of w
+def compare_match(word, w, pattern):
+    i = 0 # iterates word, the potential substring
+    j = 0 # iterates w
+
+    while True:
+        if j == len(w):
+            return False # exhausted w
+        
+        if word[i] != w[j]:
+            i = pattern[max(0,i-1)] # go back in w
+
+        if word[i] == w[j]:
+            if i == len(word) - 1:
+                return True # matched entire word
+            i = i + 1
+            j = j + 1
+        else:
+            j = j + 1 # try to find substring further along in w
+
+
+def substring_search(s: string) -> List[int]:
+    aux = [None] * len(s)
+    aux[0] = 0
+
+    j = 0
+    for i in range(1, len(s)):
+        while s[i] != s[j]:
+            if j == 0:
+                break
+            j = aux[j - 1]
+
+        if s[i] == s[j]:
+            aux[i] = j + 1
+            i = i + 1
+            j = j + 1
+        else:
+            aux[i] = 0
+            i = i + 1
+    
+    return aux
+
+
         
