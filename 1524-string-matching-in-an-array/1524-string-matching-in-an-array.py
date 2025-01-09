23ed9d148f560f2a562eaf7148e71e86ba568b56
David Costa <davidamorimc@gmail.com>
2025-01-09 00:57:38 +0000

    1524-STRING-MATCHING-IN-AN-ARRAY Stats: Time: 77 ms (5.02%), Space: 17.8 MB (34.66%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -1,33 +1,39 @@
 class Solution:
+    # KMP
     def stringMatching(self, words: List[str]) -> List[str]:
         found = []
         for word in words:
             pattern = substring_search(word)
             for w in words:
-                if word != w and len(word) < len(w) and (not word in found) and compare_match(word, w, pattern):
+                if (
+                    word != w
+                    and len(word) < len(w)
+                    and (not word in found)
+                    and compare_match(word, w, pattern)
+                ):
                     found.append(word)
         return found
 
 
 # true if word is substring of w
 def compare_match(word, w, pattern):
-    i = 0 # iterates word, the potential substring
-    j = 0 # iterates w
+    i = 0  # iterates word, the potential substring
+    j = 0  # iterates w
 
     while True:
         if j == len(w):
-            return False # exhausted w
-        
+            return False  # exhausted w
+
         if word[i] != w[j]:
-            i = pattern[max(0,i-1)] # go back in w
+            i = pattern[max(0, i - 1)]  # go back in w
 
         if word[i] == w[j]:
             if i == len(word) - 1:
-                return True # matched entire word
+                return True  # matched entire word
             i = i + 1
             j = j + 1
         else:
-            j = j + 1 # try to find substring further along in w
+            j = j + 1  # try to find substring further along in w
 
 
 def substring_search(s: string) -> List[int]:
@@ -48,8 +54,5 @@ def substring_search(s: string) -> List[int]:
         else:
             aux[i] = 0
             i = i + 1
-    
+
     return aux
-
-
-        
